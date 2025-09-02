2f4d7dce92c6e03cdf1824b388cebc046eaf6ee0
Maria Eduarda <mariaeduardafbraz@outlook.com>
2025-09-02 18:50:41 -0300

    feat: cria projeto e suas dependencias

---
+++ src/main/java/com/eduardafbz/moviesbooksrecommendations/MoviesbooksrecommendationsApplication.java
@@ -0,0 +1,13 @@
+package com.eduardafbz.moviesbooksrecommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesbooksrecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesbooksrecommendationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardafbz/moviesbooksrecommendations/MoviesbooksrecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardafbz.moviesbooksrecommendations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesbooksrecommendationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
